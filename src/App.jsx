7fe67d97c996c1942c0b2f91aaecaa19db4783da
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-13 15:44:43 +0100

    (useEffect) display list of characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://ih-crud-api.herokuapp.com/characters")
+      .then((response) => {
+        setCharacters(response.data)
+      })
+      .catch((error) => {
+        console.log("Error getting characters from the API...")
+        console.log(error)
+      })
+  }, []);
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Characters App</h1>
+      
+      { characters && <h2>Number of characters: {characters.length}</h2> }
+  
+      {characters === null
+        ? <p>loading...</p>
+        : characters.length && characters.map((characterDetails, index) => {
+          return (
+            <section key={index} className='card'>
+              <h3>{characterDetails.name}</h3>
+              <p>Occupation: {characterDetails.occupation}</p>
+            </section>
+          )
+        })}
     </>
+    
   )
 }
 
